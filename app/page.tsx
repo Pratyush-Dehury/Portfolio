037f1146c00e21342218b34dc6770b6bfa9132f0
Pratyush Dehury <98162702+pratyushd0308@users.noreply.github.com>
2023-12-12 17:21:14 +0530

    Added encryption, projects and footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+import React from "react";
+import Encryption from "@/components/main/encryption";
 import Hero from "../components/main/hero";
 import Skills from "@/components/main/skills";
+import Projects from "@/components/main/projects";
+import Footer from "@/components/main/footer";
 
 export default function Home() {
   return (
@@ -7,6 +13,9 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
+        <Encryption />
+        <Projects />
+        <Footer />
       </div>
     </main>
   );
